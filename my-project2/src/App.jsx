99c35d1f91f547318da519a3d9e07052719bb60f
gyuhaaa <vkvkvk2892@gmail.com>
2024-05-08 13:34:46 +0900

    Component Practice

--- my-project2/src/App.jsx
+++ my-project2/src/App.jsx
@@ -1,27 +1,29 @@
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
     <div className="bg-red-100 min-h-screen max-w-screen-md mx-auto flex flex-col">
-      {/* 헤더 */}
+      {/* Header */}
       <header className="bg-blue-100 px-2 py-4 flex justify-between">
-        {/* 로고 */}
+        {/* Logo */}
         <div className="bg-purple-100">로고</div>
-        {/* 네비바 */}
+        {/* NavBar */}
         <nav className="hidden md:flex gap-4">
-          {/* 버튼1 */}
+          {/* Button1 */}
           <button className="button-style">메뉴1</button>
-          {/* 버튼2 */}
+          {/* Button2 */}
           <button className="button-style">메뉴2</button>
-          {/* 버튼3 */}
+          {/* Button3 */}
           <button className="button-style">로그인</button>
         </nav>
-        {/* 더보기 버튼 */}
+        {/* ButtonMore */}
         <button className="button-style inline-block md:hidden">...</button>
       </header>
-      {/* 섹션 */}
+      {/* Section */}
       <section className="bg-green-100 grow px-2">
-        {/* 카드 리스트 */}
+        {/* CardList */}
         <ul className="bg-red-100 mt-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 justify-items-center gap-4">
-          {/* 카드 */}
+          {/* Card */}
           <li className="bg-blue-100 w-44 h-60">card</li>
           <li className="bg-blue-100 w-44 h-60">card</li>
           <li className="bg-blue-100 w-44 h-60">card</li>
@@ -34,8 +36,7 @@ const App = () => {
           <li className="bg-blue-100 w-44 h-60">card</li>
         </ul>
       </section>
-      {/* 푸터 */}
-      <footer className="bg-yellow-100 px-2">footer</footer>
+      <Footer />
     </div>
   );
 };
